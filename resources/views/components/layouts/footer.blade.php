e6fd5a6fe792accdac5114984c8de636042657e5
Hadi Santoso <hadisnt15@gmail.com>
2026-01-05 16:52:47 +0800

    update surprize 2

--- resources/views/components/layouts/footer.blade.php
+++ resources/views/components/layouts/footer.blade.php
@@ -1,8 +1,13 @@
 <div class="bottom-0 left-0 w-full z-50 pointer-events-none">
-    <footer class="bg-gray-800 rounded-base shadow-xs border border-default m-4 text-white">
-        <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-            <span class="text-sm sm:text-center">© 2026 <a href="https://flowbite.com/" class="hover:underline">Tenggo Society</a>. All Rights Reserved.
-            </span>
-        </div>
-    </footer>
+  <footer class="bg-gray-800 rounded-base shadow-xs border border-default m-4 text-white">
+    <div class="w-full mx-auto max-w-screen-xl p-4 flex justify-center items-center">
+      <span class="text-sm sm:text-center">
+        © <span id="year"></span> <a href="https://flowbite.com/" class="hover:underline">Tenggo Society</a>. All Rights Reserved.
+      </span>
+    </div>
+  </footer>
 </div>
+
+<script>
+  document.getElementById('year').textContent = new Date().getFullYear();
+</script>
--- resources/views/livewire/home/home-page.blade.php
+++ resources/views/livewire/home/home-page.blade.php
@@ -12,8 +12,8 @@
                                 <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                             </a>
                         @else
-                            <div class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
-                                <span id="countdown"></span>&nbsp;Menuju Ulang Tahun Dini 🎂
+                            <div class="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-base text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
+                                Hitung Mundur&nbsp;<span id="countdown" class="font-bold text-pink-500"></span>&nbsp;Menuju Ulang Tahun &nbsp;<span class="font-bold text-pink-500">Dini 🎂</span>
                             </div>
                         @endif
                     @else
