033b1dd1adec220bf07bae8e68cd7ff94368484e
Hadi Santoso <hadisnt15@gmail.com>
2026-01-09 11:21:13 +0800

    update sortir laughtale

--- app/Livewire/Gallery/LaughTalePage.php
+++ app/Livewire/Gallery/LaughTalePage.php
@@ -10,7 +10,7 @@ class LaughTalePage extends Component
     public function render()
     {
         return view('livewire.gallery.laugh-tale-page', [
-            'laughtales' => Gallery::orderBy('date','desc')->orderBy('id','desc')->get()
+            'laughtales' => Gallery::orderBy('id','desc')->get()
         ])
         ->layout('components.layouts.app', [
             'title' => 'JoyFinory - Our Laughtales'
